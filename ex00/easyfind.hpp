b55969dea0c64acbd42212c7b12adef0f26d7dbf
Aisyah Mokhtar <aimokhta@u80z06s02.42kl.edu.my>
2025-12-04 12:57:19 +0800

    fix: done ex00 :)

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,14 +6,76 @@
 /*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:46:55 by aimokhta          #+#    #+#             */
-/*   Updated: 2025/12/02 22:07:57 by aimokhta         ###   ########.fr       */
+/*   Updated: 2025/12/04 12:51:19 by aimokhta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <algorithm>
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
 
-template <typename T>
-void easyfind(T a, int b)
+// COLOURS
+# define CYAN			"\033[1;36m"
+# define PURPLE			"\033[1;35m"
+# define BLUE			"\033[1;34m"
+# define YELLOW			"\033[1;33m"
+# define GREEN			"\033[1;32m"
+# define RED			"\033[1;31m"
+# define BLACK			"\033[1;30m"
+# define WHITE			"\033[1;37m"
+# define LIGHT_RED		"\033[0;91m"
+# define LIGHT_GREEN	"\033[0;92m"
+# define LIGHT_YELLOW	"\033[0;93m"
+# define LIGHT_BLUE		"\033[0;94m"
+# define LIGHT_MAGENTA	"\033[0;95m"
+# define LIGHT_CYAN		"\033[0;96m"
+# define GRAY			"\033[0;90m"
+# define RESET			"\033[0m"
+# define RL_CYAN		"\001\033[1;36m\002"
+# define RL_COLOR		"\001\033[0m\002"
+
+// HEADERS
+# include <algorithm>	// std::find
+# include <iostream>	// std::cerr, std::cout 
+// 		sequence containers
+# include <vector>		// std::vector
+# include <list>		// std::list
+# include <deque>		// std::deque
+//		associative containers
+# include <map>			// std::map, std::multimap
+# include <set>			// std::set, std::multiset
+//		container adapters
+# include <stack>		// std::stack
+# include <queue>		// std::queue, std::priority_queue
+
+template <typename ContainerT>
+typename ContainerT::iterator easyfind(ContainerT &container, const int &num)
 {
+	return std::find(container.begin(), container.end(), num);
+}
+
+template<typename ContainerT>
+void testing()
+{
+	int arr[] = {26, 30, 34, 38, 42, 45, 69, 70, 5, 5, 3, -10, -3, -23};
+	ContainerT container(arr, arr + sizeof(arr)/sizeof(int));
+	
+	for (typename ContainerT::iterator it = container.begin();
+		 it != container.end();
+		 ++it)
+	{
+		const int &element = *it;
+		std::cout << element << " at " << &element << '\n';
+	}
+
+	typename ContainerT::iterator it;
+	
+	it = easyfind(container, 26);
+	std::cout << GREEN << "Found 26 at " << &(*it) << '\n' << RESET;
+
+	it = easyfind(container, 0);
+	if (it == container.end())
+		std::cerr << RED <<"Cannot find 0, its the end() iterator instead\n" << RESET;
 	
 }
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,22 +6,103 @@
 /*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:58:13 by aimokhta          #+#    #+#             */
-/*   Updated: 2025/12/02 21:58:29 by aimokhta         ###   ########.fr       */
+/*   Updated: 2025/12/04 12:54:33 by aimokhta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-#if 0
+/*
+
+	Containers in C++ STL (exists in C++98)
+	`````````````````````
+	
+	1. Sequence Containers
+		- Vector				(auto resizable dynamic array)
+		- Deque					(Dynamic array of fixed-size arrays that allows fast insertions and deletions at both ends.)
+		- List					(doubly linked list)
+		
+	3. Container Adapters		(These are adapters around existing containers (vector, deque, list))
+		- Stack
+		- Queue
+		- Priority Queue
+
+	2. Associative Containers	(All these existed in the Standard Template Library long before C++98.)
+		- Set
+		- Map
+		- Multiset
+		- Multimap
+		
+		
+*/
+
+
+
 // ==================================================================
 // |                       my own tests								|
 // ==================================================================
-#endif
+
+void borderUltra(const char *title, const char *colour)
+{
+	std::cout
+	<< colour << "\n"
+	<< "============================================================="
+	<< "\n\t\t\t"<< title  << "\n"
+	<< "============================================================="
+	<< RESET << std::endl;		
+}
+
+void border(const char *title, const char *colour)
+{
+	std::cout << "\n" << colour << title << RESET << std::endl;		
+}
+
+int main()
+{
+	// Sequence containers
+	borderUltra("Sequence Containers", YELLOW);
+	
+	border("TEST1 - std::vector of int", YELLOW);
+	testing<std::vector<int> >();
+
+	border("TEST2 - std::deque of int", YELLOW);
+	testing<std::deque<int> >();
+
+	border("TEST3 - std::list of int", YELLOW);
+	testing<std::list<int> >();
 
 
+	
+	// The only associative containers we can test on
+	borderUltra("Associative Containers", PURPLE);
+	
+	border("TEST1 - std::set of int", PURPLE);
+	testing<std::set<int> >();
+	
+	border("TEST2 - std::multiset of int", PURPLE);
+	testing<std::set<int> >();
 
-#if 0
-// ===========================================================================
-// |				subject/evaluation sheet's given test                    |
-// ===========================================================================
-#endif
+	// cant test on them -> they need 2 arguments: key and value
+	// border("TEST1 - std::map of int", PURPLE);
+	// testing<std::map<int> >();
+	
+	// border("TEST2 - std::multimap of int", PURPLE);
+	// testing<std::multimap<int> >();
+
+	
+	// Container Adapters
+	// -> cant test on them, they need additional arguments
+	// borderUltra("Container Adapters", PURPLE);
+	
+	// border("TEST1 - std::stack of int", BLUE);
+	// testing<std::stack<int> >();
+
+	// border("TEST2 - std::queue of int", BLUE);
+	// testing<std::queue<int> >();
+
+	// border("TEST3 - std::priority_queue of int", BLUE);
+	// testing<std::priority_queue<int> >();
+
+
+	return 0;
+}
